f124a1458173013bbaa54019a8afe476dc8a57d2
Сергей Кушнерчук <dr_coyote@mail.ru>
2024-02-11 12:22:25 +0300

    Refactoring

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,7 +1,5 @@
 package hw06pipelineexecution
 
-import "time"
-
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -15,34 +13,55 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 		return nil
 	}
 
-	for _, s := range stages {
-		if s == nil {
+	out := make(Bi)
+	prev := in
+	for _, stage := range stages {
+		if stage == nil {
 			continue
 		}
-		in = s(stageRunner(in, done))
+		bufChannel := make(Bi)
+		go channelWrapper(bufChannel, prev, done)
+		next := make(Bi)
+		go stageRunner(stage, bufChannel, next, done)
+		prev = next
 	}
-	return in
-}
-
-func stageRunner(in In, done In) Out {
-	output := make(Bi)
 
 	go func() {
-		defer close(output)
+		defer close(out)
 		for {
 			select {
-			case v := <-in:
-				if v == nil {
-					return
-				}
-				output <- v
 			case <-done:
 				return
-			default:
-				time.Sleep(10 * time.Millisecond)
+			case v, ok := <-prev:
+				if !ok {
+					return
+				}
+				out <- v
 			}
 		}
 	}()
 
-	return output
+	return out
+}
+
+func channelWrapper(bufChannel Bi, in In, done In) {
+	defer close(bufChannel)
+	for v := range in {
+		select {
+		case bufChannel <- v:
+		case <-done:
+			return
+		}
+	}
+}
+
+func stageRunner(stage Stage, in In, next Bi, done In) {
+	defer close(next)
+	for v := range stage(in) {
+		select {
+		case next <- v:
+		case <-done:
+			return
+		}
+	}
 }
