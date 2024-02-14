49b9ad678c1e9c7a5184e7b35c2177fe0116ce15
Сергей Кушнерчук <dr_coyote@mail.ru>
2024-02-14 12:32:36 +0300

    Init commit

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/schollz/progressbar/v3" //nolint:depguard
 )
 
 var (
@@ -10,6 +15,51 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	info, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	if !info.Mode().IsRegular() {
+		return fmt.Errorf("%w: non-regular file %v", ErrUnsupportedFile, fromPath)
+	}
+
+	fileFrom, err := os.OpenFile(fromPath, os.O_RDONLY, 0o666)
+	if err != nil {
+		return fmt.Errorf("%w: %w", ErrUnsupportedFile, err)
+	}
+	defer func() { _ = fileFrom.Close() }()
+
+	size := info.Size()
+
+	if offset > size {
+		return fmt.Errorf("%w: offset %d, file size: %d", ErrOffsetExceedsFileSize, offset, size)
+	}
+
+	if limit == 0 {
+		limit = size
+	}
+	if (size - offset) < limit {
+		limit = size - offset
+	}
+
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = fileTo.Close() }()
+
+	if _, err = fileFrom.Seek(offset, io.SeekStart); err != nil {
+		return err
+	}
+
+	bar := progressbar.DefaultBytes(limit, "copy")
+	defer func() { _ = bar.Close() }()
+
+	_, err = io.CopyN(io.MultiWriter(fileTo, bar), fileFrom, limit)
+	if err != nil {
+		_ = os.Remove(toPath)
+		return err
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,123 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require" //nolint:depguard
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	in := "in.txt"
+	out := "out.txt"
+	data := []byte(
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus nibh nec metus lobortis ullamcorper.",
+	)
+
+	t.Run("full copy", func(t *testing.T) {
+		prepare(t, in, data)
+		defer purge(in, out)
+
+		err := Copy(in, out, 0, 0)
+
+		require.NoError(t, err)
+		assert(t, out, data)
+	})
+
+	t.Run("offset 10 limit 0", func(t *testing.T) {
+		prepare(t, in, data)
+		defer purge(in, out)
+
+		err := Copy(in, out, 10, 0)
+
+		require.NoError(t, err)
+		assert(t, out, data[10:])
+	})
+
+	t.Run("offset 0 limit 10", func(t *testing.T) {
+		prepare(t, in, data)
+		defer purge(in, out)
+
+		err := Copy(in, out, 0, 10)
+
+		require.NoError(t, err)
+		assert(t, out, data[:10])
+	})
+
+	t.Run("offset 10 limit 10", func(t *testing.T) {
+		prepare(t, in, data)
+		defer purge(in, out)
+
+		err := Copy(in, out, 10, 10)
+
+		require.NoError(t, err)
+		assert(t, out, data[10:20])
+	})
+
+	t.Run("offset exceeds file size", func(t *testing.T) {
+		prepare(t, in, data)
+		defer purge(in, out)
+
+		err := Copy(in, out, int64(len(data)+10), 0)
+		require.True(
+			t,
+			errors.Is(err, ErrOffsetExceedsFileSize),
+			"offset exceeds file size: offset 100, file size: 13",
+		)
+	})
+
+	t.Run("offset with limit exceeds file size", func(t *testing.T) {
+		prepare(t, in, data)
+		defer purge(in, out)
+
+		err := Copy(in, out, int64(len(data)-10), 20)
+		require.NoError(t, err)
+		assert(t, out, data[len(data)-10:])
+	})
+
+	t.Run("unsupported file as source", func(t *testing.T) {
+		err := Copy("/dev/random", out, 0, 0)
+
+		require.True(
+			t,
+			errors.Is(err, ErrUnsupportedFile),
+			"unsupported file: non-regular file /dev/random",
+		)
+	})
+
+	t.Run("directory as source", func(t *testing.T) {
+		err := Copy("/tmp", out, 0, 0)
+
+		require.True(
+			t,
+			errors.Is(err, ErrUnsupportedFile),
+			"unsupported file: non-regular file /tmp",
+		)
+	})
+
+	t.Run("source does not exist", func(t *testing.T) {
+		err := Copy("/tmp/does-not-exist", out, 0, 0)
+
+		require.Equal(t, "stat /tmp/does-not-exist: no such file or directory", err.Error())
+	})
+}
+
+func prepare(t *testing.T, inFilePath string, data []byte) {
+	t.Helper()
+	err := os.WriteFile(inFilePath, data, os.ModePerm)
+	require.NoError(t, err)
+}
+
+func purge(files ...string) {
+	for _, file := range files {
+		_ = os.Remove(file)
+	}
+}
+
+func assert(t *testing.T, outFilePath string, expected []byte) {
+	t.Helper()
+	data, err := os.ReadFile(outFilePath)
+	require.NoError(t, err)
+	require.Equal(t, expected, data)
 }
